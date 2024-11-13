ac8ef34ae658da26446fe790c6e9ba49319a7955
fortishield <161459699+FortiShield@users.noreply.github.com>
2024-11-13 10:26:49 +0600

    Update lib/nephrase/plugin/translate_gpt.rb
    Co-authored-by: codiumai-pr-agent-pro[bot] <151058649+codiumai-pr-agent-pro[bot]@users.noreply.github.com>
    Signed-off-by: fortishield <161459699+FortiShield@users.noreply.github.com>

--- lib/nephrase/plugin/translate_gpt.rb
+++ lib/nephrase/plugin/translate_gpt.rb
@@ -4,7 +4,7 @@ module Nephrase
   module TranslateGpt
     # Return all .rb files inside the "actions" and "helper" directory
     def self.all_classes
-      Dir[File.expand_path('**/{actions,helper}/*.rb', File.dirname(__FILE__))]
+      @all_classes ||= Dir[File.expand_path('**/{actions,helper}/*.rb', File.dirname(__FILE__))]
     end
   end
 end
